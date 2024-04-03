0750b9ef27d76483d1984071b8f079067c3da3a3
csm031 <csm031@naver.com>
2024-04-03 10:01:04 +0900

    04.03 오전1

---
+++ src/main/java/human/web/member/MemberLogoutServlet.java
@@ -0,0 +1,24 @@
+package human.web.member;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/member/memberLogOut")
+public class MemberLogoutServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    //로그아웃을 처리하는 메소드
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        //기존의 세션객체에 저장된 내용을 초기화시킴: invalidate()
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        //메인 페이지로 이동
+        response.sendRedirect("../index.jsp");
+    }
+}
