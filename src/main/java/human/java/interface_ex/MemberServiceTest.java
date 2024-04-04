d95b16c0c72b23dab88f0d0272e1bf9a349594d5
csm031 <csm031@naver.com>
2024-04-04 19:17:00 +0900

    04.04 보충

--- src/main/java/human/java/interface_ex/MemberServiceTest.java
+++ src/main/java/human/java/interface_ex/MemberServiceTest.java
@@ -15,5 +15,9 @@ public class MemberServiceTest {
         MemberDTO dto = new MemberDTO();
         mJoin.join(dto);
         mLogin.login("hong", "1234");
+
+        //상위계층의 인터페이스형으로 참조변수의 타입을 정의하고 하위 클래스 내에 메소드
+        //오버라이딩된 메소드를 호출하면 참조타입변환 없이 하위클래스의 오버라이드한 메소드
+        //를 호출할 수 있음
     }
 }
